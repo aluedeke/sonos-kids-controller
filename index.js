0e40922e69fded72ec17a308f8ea74d157f03d0c
aluedeke <andreas@ludeke.net>
2018-01-15 14:37:19 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,96 @@
+var restify = require('restify');
+var sonos = require('./sonos.js');
+
+var server = restify.createServer();
+server.use(restify.plugins.queryParser());
+
+server.get('/play', function create(req, res, next) {
+    console.log('play');
+
+    var playlist = req.query.playlist;
+    var zone = req.query.zone;
+
+    sonos.playPlaylist(zone, playlist);
+
+    res.send(200);
+
+    return next();
+});
+
+server.get('/stop', function create(req, res, next) {
+    console.log('stop');
+    
+    var zone = req.query.zone;
+
+    sonos.stop(zone);
+
+    res.send(200);
+
+    return next();
+});
+
+server.listen(8080, '0.0.0.0', function() {
+    console.log('%s listening at %s', server.name, server.url);
+});
+
+// var search = sonos.search(10000, function(s){
+//     if(s.host != '192.168.178.77'){
+//         return;
+//     }
+    // console.log(s);
+
+    // s.deviceDescription((err, info) => {
+    //     //console.log(err ? err : info.coordinator);
+    // })
+
+    // s.getZoneAttrs(function (err, info) {
+    //     //console.log(err ? err : info);
+    // })
+
+    // s.getZoneInfo(function (err, info) {
+    //     //console.log(err ? err : info);
+    // })
+
+    // s.getTopology(fun192.168.178.77:1400/xml/device_description.xml',ction (err, info) {
+    //     //console.log(err ? err : info);
+    // })
+
+    // s.searchMusicLibra    // s.stop((err, data) => {
+    //     console.log(err ? err : data);
+    // });ry('sonos_playlists', null, {start:0, end:100}, function (err, data) {
+    //     console.log(data)
+    // })
+
+    // s.play('file:///jffs/settings/savedqueues.rsq#1', function(err, playing){
+    //     console.log(err);
+    // });
+    
+    // s.getCurrentState((err, data) => {
+    //     console.log(err ? err : data);
+    // });
+
+    // s.flush((err, data) => {
+    //     console.log(err ? err : data);
+    // });
+
+    // s.getQueue((err, data) => {
+    //     console.log(err ? err : data);
+    // });
+
+    // s.stop((err, data) => {
+    //     console.log(err ? err : data);
+    // });
+
+    // s.play((err, data) => {
+    //     console.log(err ? err : data);/home/aluedeke/Development/source/private/sonos-kids-controller/node-controller/node_modules/sonos/node_modules/xml2js/lib/parser.js:261:26
+    // });
+
+    // device.currentTrack(console.log);
+
+    // s.selectQueue((err, data) => {
+    //     console.log(err ? err : data);
+    //     s.play((err, data) => {
+    //         console.log(err ? err : data);
+    //     });
+    // });
+// });
---
+++ sonos.js
@@ -0,0 +1,106 @@
+var api = require('sonos');
+
+var zones = {}
+
+refreshZones()
+
+setInterval(() => {
+    refreshZones()
+}, 15000)
+
+function refreshZones(){
+    api.search({timeout: 10000}, async (player) => {
+        var topology = await getTopology(player)
+
+        var location = 'http://' + player.host + ':' + player.port + '/xml/device_description.xml'
+        topology.zones.filter(zone => zone.location == location).forEach(zone => {
+            if(!zone.coordinator){
+                return;
+            }
+
+            zones[zone.name] = player
+        });
+    });
+}
+
+async function getTopology(player){
+    return new Promise((resolve, reject) => {
+        player.getTopology((error, topology) => {
+            return void error ? reject(error) : resolve(topology)
+        })
+    })
+}
+
+exports.playPlaylist = async function(zone, playlist){
+    var player = zones[zone];
+
+    var playlist = await getPlaylist(player, playlist);
+
+    await flush(player)
+    await queue(player, playlist.uri)
+    await selectQueue(player)
+    await play(player)
+}
+
+async function getPlaylist(player, name){
+    return new Promise((resolve, reject) => {
+        player.searchMusicLibrary('sonos_playlists', null, {start:0, end:100}, (error, result) => {
+            console.log('get playlist: ' + JSON.stringify(error ? error : result));
+            return void error ? reject(error) : resolve(result.items.filter(p => name == p.title)[0])
+        });
+    })
+}
+
+async function flush(player){
+    return new Promise((resolve, reject) => {
+        player.flush((error, data) => {
+            console.log('flushed: ' + JSON.stringify(error ? error : data));
+            return void error ? reject(error) : resolve(data)
+        });
+    })
+}
+
+async function queue(player, uri){
+    return new Promise((resolve, reject) => {
+        player.queue(uri, (error, queued) => {
+            console.log('queued: ' + JSON.stringify(error ? error : queued));
+            return void error ? reject(error) : resolve(queued)
+        });
+    })
+}
+
+async function selectQueue(player){
+    return new Promise((resolve, reject) => {
+        player.selectQueue((error, data) => {
+            console.log('queue selected: ' + JSON.stringify(error ? error : data));
+            return void error ? reject(error) : resolve(data)
+        });
+    })
+}
+
+async function play(player){
+    return new Promise((resolve, reject) => {
+        player.play((error, data) => {
+            console.log('play: ' + JSON.stringify(error ? error : data));
+            return void error ? reject(error) : resolve(data)
+        });
+    })
+}
+
+exports.stop = async function(zone){
+    var player = zones[zone];
+
+    await stop(player);
+}
+
+async function stop(player){
+    return new Promise((resolve, reject) => {
+        player.stop((error, stopped) => {
+            return void error ? reject(error) : resolve(stopped)
+        });
+    })
+}
+
+
+
+    
